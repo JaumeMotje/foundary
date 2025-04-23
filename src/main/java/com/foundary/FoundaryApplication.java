d6a965ffe0bffd6ad07157449059792b83ea5f5d
Jaume Motje <jaumemotje00@gmail.com>
2025-04-23 18:36:42 +0200

    Spring Boot Template

---
+++ src/main/java/com/foundary/FoundaryApplication.java
@@ -0,0 +1,13 @@
+package com.foundary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foundary/FoundaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foundary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
